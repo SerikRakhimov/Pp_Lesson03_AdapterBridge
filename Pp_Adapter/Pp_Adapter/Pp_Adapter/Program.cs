25347a62299aa1a10b90a26824061fb00e34b0e9
RSB <s_astana@mail.ru>
2019-07-24 04:59:15 +0600

    first commit

---
+++ Pp_Adapter/Pp_Adapter/Pp_Adapter/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Adapter
+
+namespace Pp_Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // продукт
+            Product product = new Product();
+            // фрукт
+            Fruit fruit = new Fruit();
+            // готовим блюдо
+            product.Food(fruit);
+            // появилась ягода
+            Kryzhovnik kryzhovnik = new Kryzhovnik();
+            // используем адаптер
+            IFruit kryzhovnikFruit = new KryzhovnikToFruitAdapter(kryzhovnik);
+            // продолжаем готовить с крыжовником
+            product.Food(kryzhovnikFruit);
+
+            Console.ReadLine();
+        }
+    }
+    interface IFruit
+    {
+        void Gotovka();
+    }
+    // класс машины
+    class Fruit : IFruit
+    {
+        public void Gotovka()
+        {
+            Console.WriteLine("Из фруктов получается варенье");
+        }
+    }
+    class Product
+    {
+        public void Food(IFruit fruit)
+        {
+            fruit.Gotovka();
+        }
+    }
+    // интерфейс ягоды
+    interface IYagoda
+    {
+        void Varka();
+    }
+    // класс крыжовника
+    class Kryzhovnik : IYagoda
+    {
+        public void Varka()
+        {
+            Console.WriteLine("Из крыжовника получается желе");
+        }
+    }
+    // Адаптер от Kryzhovnik к IFruit
+    class KryzhovnikToFruitAdapter : IFruit
+    {
+        Kryzhovnik kryzhovnik;
+        public KryzhovnikToFruitAdapter(Kryzhovnik c)
+        {
+            kryzhovnik = c;
+        }
+
+        public void Gotovka()
+        {
+            kryzhovnik.Varka();
+        }
+    }
+
+}
---
+++ Pp_Bridge/Pp_Bridge/Pp_Bridge/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Bridge
+
+namespace Pp_Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем новый пункт меню, на русском языке
+            Console.WriteLine("На русском языке:");
+            Menu menu = new AboutMenu(new Russian());
+            menu.DoWork();
+            Console.WriteLine("\nНа английском языке:");
+            // теперь нужно на английском
+            menu.Language = new English();
+            menu.DoWork();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ILanguage
+    {
+        void Small();
+        void Full();
+    }
+
+    class Russian : ILanguage
+    {
+        public void Small()
+        {
+            Console.WriteLine("О программне");
+        }
+
+        public void Full()
+        {
+            Console.WriteLine("Версия программы 1.0.0.0.");
+        }
+    }
+
+    class English : ILanguage
+    {
+        public void Small()
+        {
+            Console.WriteLine("About the program");
+        }
+
+        public void Full()
+        {
+            Console.WriteLine("Version of the program 1.0.0.0.");
+        }
+    }
+
+    abstract class Menu
+    {
+        protected ILanguage language;
+        public ILanguage Language
+        {
+            set { language = value; }
+        }
+        public Menu(ILanguage lang)
+        {
+            language = lang;
+        }
+        public virtual void DoWork()
+        {
+            language.Small();
+            language.Full();
+        }
+    }
+
+    class AboutMenu : Menu
+    {
+        public AboutMenu(ILanguage lang) : base(lang)
+        {
+        }
+    }
+    class CorporateProgrammer : Menu
+    {
+        public CorporateProgrammer(ILanguage lang)
+            : base(lang)
+        {
+        }
+    }
+
+}
